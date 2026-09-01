ac832ab80b75a0af4f30e3dc858362ab75d7df0b
marczydricabraham.stratpoint <marczydric.abraham@stratpoint.com>
2026-09-02 06:53:30 +0800

    Script for sending a payload to the webhook

---
+++ scripts/send-payload.sh
@@ -0,0 +1,8 @@
+You can change the payload you want to use:
+
+1. "curl -X POST http://localhost:5678/webhook-test/signup-hook \
+  -H "Content-Type: application/json" \
+  -d @payloads/users-corporate-public.json"
+  
+2. "curl -X POST http://localhost:5678/webhope: application/json" -d @payloads/users-batch-mixed.json"
+
